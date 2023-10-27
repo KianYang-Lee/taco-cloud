63f90ca627b3a8946b14b2c5d8645bb751b032e3
kianyang_lee <ken.lee.kianyang@gmail.com>
2023-10-27 11:38:43 +0800

    chap 6 extracted configuration to configuration property holder bean

--- src/main/java/io/kianyanglee/tacos/controllers/OrderController.java
+++ src/main/java/io/kianyanglee/tacos/controllers/OrderController.java
@@ -2,7 +2,6 @@ package io.kianyanglee.tacos.controllers;
 
 import java.util.List;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -15,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 
+import io.kianyanglee.tacos.OrderProps;
 import io.kianyanglee.tacos.domain.TacoOrder;
 import io.kianyanglee.tacos.domain.User;
 import io.kianyanglee.tacos.repositories.OrderRepository;
@@ -25,20 +25,14 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 @RequestMapping("/orders")
 @SessionAttributes("tacoOrder")
-@ConfigurationProperties(prefix = "taco.orders")
 public class OrderController {
     
-    private int pageSize = 20;
-
-    public void setPageSize(int pageSize) {
-        this.pageSize = pageSize;
-    }
-
     private OrderRepository orderRepository;
+    private OrderProps orderProps;
 
-
-    public OrderController(OrderRepository orderRepository) {
+    public OrderController(OrderRepository orderRepository, OrderProps orderProps) {
         this.orderRepository = orderRepository;
+        this.orderProps = orderProps;
     }
 
     @GetMapping("/current")
@@ -63,7 +57,7 @@ public class OrderController {
 
     @GetMapping
     public String ordersForUser(@AuthenticationPrincipal User user, Model model) {
-        Pageable pageable = PageRequest.of(0, pageSize);
+        Pageable pageable = PageRequest.of(0, orderProps.getPageSize());
         List<TacoOrder> tacoOrderList = orderRepository.findByUserOrderByPlacedAtDesc(user, pageable);
         log.info("Taco list is : {}", tacoOrderList);
         model.addAttribute("orders", tacoOrderList);
